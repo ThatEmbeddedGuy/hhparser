e2c51330ea82577be520b332235da700a98bdf37
kirill tikhonov <slimline@inbox.ru>
2020-07-04 21:38:59 +0300

    add TODO, firmat

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
+extern crate reqwest;
 extern crate tokio;
 extern crate tokio_io;
-extern crate reqwest;
 
 mod parser;
 mod request;
 
 const TITLE: &str = "C++";
 
+async fn parse_hh_index() {
+    let first_page = request::get_page(0, TITLE).await;
+    let pages = parser::get_num_of_pages(&first_page);
 
-async fn parse_hh()
-{
-  parser::get_num_of_pages("dd");
-  print!("{}",request::get_page(0,TITLE).await);
+    // TODO add return value (list of vacancies)
 }
-
 
 #[tokio::main]
 async fn main() {
-  parse_hh().await;
+    parse_hh_index().await;
+    //TODO filter already cached vacancies
+    //TODO perform requests for new vacancies
 }
--- src/request.rs
+++ src/request.rs
@@ -1,32 +1,26 @@
-extern crate  urlencoding;
+extern crate urlencoding;
 
-pub async fn get_page(_num: i32, _keyword:  &str ) -> String
-{
-  let mut request_uri = REQUEST_URI.to_string() + &urlencoding::encode(_keyword) ;
-  if _num !=0
-  {
-    request_uri.push_str(PAGE_PARAM);
-    request_uri.push_str(&_num.to_string());
-  }
-  let res = make_request(&request_uri).await;
-  match res { 
-  Ok(x) =>  x,
-  _ => "".to_string()
-  }
+pub async fn get_page(_num: i32, _keyword: &str) -> String {
+    let mut request_uri = REQUEST_URI.to_string() + &urlencoding::encode(_keyword);
+    if _num != 0 {
+        request_uri.push_str(PAGE_PARAM);
+        request_uri.push_str(&_num.to_string());
+    }
+    let res = make_request(&request_uri).await;
+    match res {
+        Ok(x) => x,
+        _ => "".to_string(),
+    }
 }
 
-
-async fn make_request(url: &String) ->  Result<String, reqwest::Error>
-{
+async fn make_request(url: &String) -> Result<String, reqwest::Error> {
     let resp = reqwest::get(url).await?;
-    match resp.error_for_status()
-    {
-             Ok(_res) =>  return Ok(_res.text().await?.to_string()),
-             Err(err) => Err(err)
-   }
+    match resp.error_for_status() {
+        Ok(_res) => return Ok(_res.text().await?.to_string()),
+        Err(err) => Err(err),
+    }
 }
 
-
-
+//TODO use hh api
 const REQUEST_URI : &str = "https://spb.hh.ru/search/vacancy?clusters=true&area=2&enable_snippets=true&salary=&st=searchVacancy&text=";
-const PAGE_PARAM : &str = "&page=";
+const PAGE_PARAM: &str = "&page=";
