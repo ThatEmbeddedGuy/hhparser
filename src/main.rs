2f5cacce7ef6d54ac53892a41cdd7903383f0e93
kirill tikhonov <slimline@inbox.ru>
2020-07-05 21:15:35 +0300

    add cli parameters

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate futures;
 extern crate reqwest;
 extern crate tokio;
 extern crate tokio_io;
+extern crate clap;
+
+use clap::Clap;
 
 mod parser;
 mod request;
@@ -23,7 +26,7 @@ async fn get_rest_pages(
         let mut page = 0;
         index_pages_futures.resize_with(_pages as usize, || {
             page += 1;
-            request::get_page(page,  _keyword.clone())
+            request::get_page(page, _keyword.clone())
         });
         index_pages_futures
     };
@@ -31,18 +34,39 @@ async fn get_rest_pages(
     index_pages
 }
 
+/// headhunter parser 
+#[derive(Clap)]
+#[clap(version = "0.1", author = "Kirrill Tikhonov  <yaslimline@gmail.com>")]
+struct Opts {
+    /// Search keyword
+    #[clap(short, long, default_value = "C++")]
+    keyword: String,
+    /// export_only - omit search
+    #[clap(short, long)]
+    export_only: bool,
+    /// Export format ( default - print output)
+    #[clap(short, long, default_value = "print")]
+    fmt: String,
+}
+
+
+
+
 #[tokio::main]
 async fn main() {
+    let opts: Opts = Opts::parse();
     println!("Started");
-    // TODO parse CL arguments (get, clean DB, export)
-    if let Some(first) = get_first_page(TITLE).await {
+    let search_keyword = opts.keyword;
+        
+    if let (false, Some(first)) = (opts.export_only, get_first_page(&search_keyword).await) {
         let pages = parser::parse_num_of_pages(&first);
         println!("num of pages parsed: {}", pages);
-        let _tasks = get_rest_pages(&TITLE.to_string(), pages).await;
+        let _tasks = get_rest_pages(&search_keyword, pages).await;
         let _index_pages = futures::future::join_all(_tasks).await;
         //TODO filter already cached vacancies
-        //TODO perform requests for new vacancies
     }
+
+    //TODO export
 
     println!("Finished");
 }
