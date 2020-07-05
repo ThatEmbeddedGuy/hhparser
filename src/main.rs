6c019c8e2f11e8d86ada4cdf3da2e1fcce1c7a20
kirill tikhonov <slimline@inbox.ru>
2020-07-05 13:56:10 +0300

    add logs

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,16 @@ const TITLE: &str = "C++";
 async fn parse_hh_index() {
     if let Some(json) = request::get_page(0, TITLE).await.and_then(parser::parse_json_own) {
         let _pages = parser::parse_num_of_pages(&json);
+        println!("First page parsed, num of pages: {}", _pages);
     }
     // TODO add return value (list of vacancies)
 }
 
 #[tokio::main]
 async fn main() {
+    println!("Started");
     parse_hh_index().await;
     //TODO filter already cached vacancies
     //TODO perform requests for new vacancies
+    println!("Finished");
 }
