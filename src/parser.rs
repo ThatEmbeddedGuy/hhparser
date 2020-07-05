9870a43e0346abebe524e086c18fd4cdf9031697
kirill tikhonov <slimline@inbox.ru>
2020-07-05 13:52:59 +0300

    json parser tests

--- src/parser.rs
+++ src/parser.rs
@@ -12,7 +12,7 @@ pub fn parse_json(_body: &str) -> Option<serde_json::Value> {
 
 #[allow(dead_code)]
 pub fn parse_num_of_pages(root: &serde_json::Value) -> u64 {
-    if let Some(value) = root.get("Pages").and_then(|node| node.as_u64()) {
+    if let Some(value) = root.get("pages").and_then(|node| node.as_u64()) {
         value
     } else {
         0
@@ -30,6 +30,16 @@ pub fn get_num_of_pages(_body: &str) -> u64 {
 }
 
 #[test]
-fn num_pages_test() {
-    assert_eq!(get_num_of_pages("some"), 11);
+fn invalid_json() {
+    assert_eq!(get_num_of_pages("some"), 0);
+}
+
+#[test]
+fn empty_string() {
+    assert_eq!(get_num_of_pages(""),0);
+}
+
+#[test]
+fn correct_json() {
+    assert_eq!(get_num_of_pages("{\"pages\": 11}"),11);
 }
